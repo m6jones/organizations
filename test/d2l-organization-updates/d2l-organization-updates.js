9c58809760cf337d3b9c87aadb072ad569ca97e8
Maya Jones <Maya.Jones@D2L.com>
2018-10-04 16:46:52 -0400

    Connect links to the updates.

--- test/d2l-organization-updates/d2l-organization-updates.js
+++ test/d2l-organization-updates/d2l-organization-updates.js
@@ -18,31 +18,155 @@ describe('d2l-organization-updates', () => {
 		sandbox = sinon.sandbox.create();
 
 		notificationEntity = {
-			properties: {
-				UnattemptedQuizzes: -20,
-				UnreadAssignmentFeedback: 0,
-				UngradedQuizzes: 4,
-				UnreadDiscussions: 20,
-				UnapprovedDiscussions: 79,
-				UnreadAssignmentSubmissions: 200
-			},
+			entities: [{
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 20,
+					type: 'UnreadDiscussions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadDiscussions',
+					href: '/discussions'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 79,
+					type: 'UnapprovedDiscussions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnapprovedDiscussions',
+					href: '/discussions'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 0,
+					type: 'UnreadAssignmentFeedback'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadAssignmentFeedback',
+					href: '/assignment'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 200,
+					type: 'UnreadAssignmentSubmissions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadAssignmentSubmissions',
+					href: '/assignment'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 4,
+					type: 'UngradedQuizzes'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UngradedQuizzes',
+					href: '/quizzes'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: -20,
+					type: 'UnattemptedQuizzes'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnattemptedQuizzes',
+					href: '/quizzes'
+				}]
+			}],
 			links: [{
 				rel: ['self'],
 				href: '/data/notification.json'
+			}, {
+				rel: ['https://api.brightspace.com/rels/organization'],
+				href: 'https://98739553-44af-4933-b09c-f3798cdb13f5.organizations.api.proddev.d2l/6609'
 			}]
 		};
 		notificationEntityAllFull = {
-			properties: {
-				UnattemptedQuizzes: 30,
-				UnreadAssignmentFeedback: 40,
-				UngradedQuizzes: 4,
-				UnreadDiscussions: 20,
-				UnapprovedDiscussions: 79,
-				UnreadAssignmentSubmissions: 200
-			},
+			entities: [{
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 20,
+					type: 'UnreadDiscussions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadDiscussions',
+					href: '/discussions'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 79,
+					type: 'UnapprovedDiscussions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnapprovedDiscussions',
+					href: '/discussions'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 40,
+					type: 'UnreadAssignmentFeedback'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadAssignmentFeedback',
+					href: '/assignment'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 200,
+					type: 'UnreadAssignmentSubmissions'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnreadAssignmentSubmissions',
+					href: '/assignment'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 4,
+					type: 'UngradedQuizzes'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UngradedQuizzes',
+					href: '/quizzes'
+				}]
+			}, {
+				rel: ['https://notifications.api.brightspace.com/rels/updates'],
+				properties: {
+					count: 30,
+					type: 'UnattemptedQuizzes'
+				},
+				links: [{
+					rel: ['https://notifications.api.brightspace.com/rels/updates-source'],
+					title: 'UnattemptedQuizzes',
+					href: '/quizzes'
+				}]
+			}],
 			links: [{
 				rel: ['self'],
 				href: '/data/notification.json'
+			}, {
+				rel: ['https://api.brightspace.com/rels/organization'],
+				href: 'https://98739553-44af-4933-b09c-f3798cdb13f5.organizations.api.proddev.d2l/6609'
 			}]
 		};
 		presentationEntity = {
