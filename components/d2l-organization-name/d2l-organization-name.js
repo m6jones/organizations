fc6e9c8c125bf91f0807064a2f91677192043a45
Edward Gao <Edward.Gao@D2L.com>
2019-03-19 10:37:03 -0400

    US105062-componentize organization name

--- components/d2l-organization-name/d2l-organization-name.js
+++ components/d2l-organization-name/d2l-organization-name.js
@@ -10,55 +10,49 @@ Polymer-based web component for a organization name.
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import '@polymer/polymer/polymer-legacy.js';
-
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
+import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
+import 'd2l-polymer-siren-behaviors/store/siren-action-behavior.js';
 import '../d2l-organization-behavior.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-const $_documentContainer = document.createElement('template');
 
-$_documentContainer.innerHTML = `<dom-module id="d2l-organization-name">
-	<template strip-whitespace="">
-		[[_organizationName]]
-	</template>
+/**
+ * @customElement
+ * @polymer
+ */
+class OrganizationName extends mixinBehaviors([
+	D2L.PolymerBehaviors.Siren.EntityBehavior,
+	D2L.PolymerBehaviors.Siren.SirenActionBehaviorImpl,
+	D2L.PolymerBehaviors.Organization.Behavior
+], PolymerElement) {
+	static get template() {
+		return html`
+			[[_organizationName]]
+		`;
+	}
+	static get properties() {
+		return {
+			_organizationName: {
+				type: String,
+				computed: '_getOrganizationName(entity)'
+			}
+		};
+	}
 
-	
-</dom-module>`;
+	static get observers() {
+		return [
+			'_sendVoiceReaderInfo(_organizationName)'
+		];
+	}
 
-document.head.appendChild($_documentContainer.content);
-Polymer({
-	is: 'd2l-organization-name',
+	static get is() { return 'd2l-organization-name'; }
 
-	properties: {
-		href: String,
+	_getOrganizationName(entity) {
+		console.log(entity);
+		return entity && entity.properties && entity.properties.name || '';
+	}
 
-		_organizationName: String
-	},
-
-	behaviors: [
-		D2L.PolymerBehaviors.Organization.Behavior
-	],
-
-	observers: [
-		'_fetchOrganization(href)',
-		'_sendVoiceReaderInfo(_organizationName)'
-	],
-
-	_fetchOrganization: function(organizationHref) {
-		return this._fetchSirenEntity(organizationHref)
-			.then(function(organizationEntity) {
-				if (!organizationEntity) {
-					return;
-				}
-
-				if (!organizationEntity.properties) {
-					return;
-				}
-
-				this._organizationName = organizationEntity.properties.name;
-			}.bind(this));
-	},
-
-	_sendVoiceReaderInfo: function(organizationName) {
+	_sendVoiceReaderInfo (organizationName) {
 		var details = {
 			organization: {
 				name: organizationName
@@ -66,5 +60,7 @@ Polymer({
 		};
 
 		this._fireD2lOrganizationAccessible(details);
-	},
-});
+	}
+}
+
+window.customElements.define(OrganizationName.is, OrganizationName);
